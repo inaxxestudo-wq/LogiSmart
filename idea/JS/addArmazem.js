6518b027c0bdeac5fa7a151441e577af4652ce04
inaxxestudo-wq <inaxxestudo@gmail.com>
2026-08-09 21:20:25 -0300

    Add files via upload

--- idea/JS/addArmazem.js
+++ idea/JS/addArmazem.js
@@ -3,6 +3,8 @@ function add() {
   const nome = document.getElementById("nome").value;
   const pesoMin = document.getElementById("pMin").value;
   const pesoMax = document.getElementById("pMax").value;
+  const tag = document.getElementById("selecionarCategoria").value
+
   const arquivo = foto.files[0];
 
   const leitor = new FileReader();
@@ -14,9 +16,9 @@ function add() {
       nome: nome,
       pesoMin: pesoMin,
       pesoMax: pesoMax,
-      imagem: base64
+      imagem: base64,
+      categoria: tag
     };
-
     // pega lista atual de produtos
     let lista = JSON.parse(localStorage.getItem("produtos")) || [];
 
@@ -30,5 +32,66 @@ function add() {
   };
 
   leitor.readAsDataURL(arquivo);
-  
+
+}
+
+
+function aparecer(){
+
+  document.getElementById("principalSub").classList.toggle("mov")
+  document.getElementById("adicionarppp").classList.toggle("movebtn")
+
+  document.getElementById("divSec").classList.toggle("visible")
+}
+
+function criarCategoria(){
+var categorias = JSON.parse(localStorage.getItem("categorias")) || []
+var iptCategoria = document.getElementById("inputCategoria")
+if(!categorias.includes(iptCategoria.value.toLowerCase())){
+  categorias.push(iptCategoria.value.toLowerCase())
+localStorage.setItem("categorias",JSON.stringify(categorias.sort()))
+
+var selecionarCategoria = document.getElementById("selecionarCategoria")
+var categorias = JSON.parse(localStorage.getItem("categorias")) ||[]
+categorias.sort()
+categorias.length
+
+selecionarCategoria.innerHTML += `<option value="${iptCategoria.value.toLowerCase()}">${iptCategoria.value.toLowerCase()}</option>`
+
+}
+else alert("A categoria já existe")
+
+
+}
+function apagarCategoria(){
+var categorias = JSON.parse(localStorage.getItem("categorias")) ||[]
+categorias.sort()
+
+var iptCategoria = document.getElementById("selecionarCategoria")
+if(iptCategoria.value !="0"){
+alert(iptCategoria.value)
+  for(let i = 0;i<categorias.length;i++){
+
+    if(categorias[i]==iptCategoria.value){
+
+     categorias.splice(i,1)
+      break
+    }
+  }
+localStorage.setItem("categorias",JSON.stringify(categorias))
+
+var produtos = JSON.parse(localStorage.getItem("produtos"))
+
+for(let i =0;i<produtos.length;i++){
+var item = produtos[i]
+
+if(item.categoria == iptCategoria.value)produtos.splice(i,1),i--
+
+}
+
+localStorage.setItem("produtos",JSON.stringify(produtos))
+location.reload()
+}
+
+else alert("escolha antes uma opção para apagar")
 }
--- idea/JS/vendas.js
+++ idea/JS/vendas.js
@@ -1,26 +1,213 @@
+let meuGraficoFaturamento = null
+let graficoPreju = null
+let graficoFormaPG = null
+let valores = []
+let mesExato;
+let label = []
 function carregar(){
-    var ftUser = document.getElementById("ftUserMain")
+      console.log((localStorage.getItem("vendas2/2026")))
+var ftUser = document.getElementById("ftUserMain")
 var ftUserSide = document.getElementById("ftUser")
 ftUserSide.src = localStorage.getItem(("imgUser"))
 
-
-const ctx = document.getElementById('meuGrafico');
-
-new Chart(ctx, {
-    type: 'line', // tipos: bar, line, pie, doughnut, radar...
-    data: {
-        labels: ['01', '02', '03', '04','05','07','08','09',10],
+let [dia,mes,ano] = receberData("full")
+label = criarLabel(mes,ano)
+let mesExato = mes+"/"+ano
+var entradas = JSON.parse(localStorage.getItem("vendas"+mesExato)) 
+var saidas = JSON.parse(localStorage.getItem("saidas"+mesExato))
+var faturamento = entradas-saidas 
+console.log(localStorage.getItem("pix"+mesExato)|| 0)
+ meuGraficoFaturamento = new Chart(document.getElementById("meuGraficoFaturamento"),{
+type:"bar",
+data:{
+    
+   labels: label,
         datasets: [{
-            label: 'Vendas',
-            data: [10, 20, 15, 30,50,5,10,12,11,12],
-            borderWidth: 2
-        }]
-    },
-    options: {
+            label: 'Faturamento',
+            data: faturamento,
+            borderWidth: 2,
+            
+        },
+{           label: 'Saída',
+            data: saidas},
+        {   label: 'Entrada',
+            data: entradas}]
+        
+}
+,
+  options: {
         responsive: true
-    }
-});
+}
+})
+  graficoFormaPG = new Chart(
+    document.getElementById("meuGraficoFormaPg"),
+    {
+        type: "pie",
+
+       data: {
+    labels: ["Pix", "Crédito", "Dinheiro","Débito"],
+
+    datasets: [{
+        label: "Formas de pagamento",
+        data: [localStorage.getItem("pix"+mesExato)||0,localStorage.getItem("credito"+mesExato)|| 0,localStorage.getItem("dinheiro"+mesExato)|| 0,localStorage.getItem("debito"+mesExato)||0],
+        backgroundColor: ["#ffbc0b","#2979ff","#00c853","#361f65"],
+        borderColor: "#ffffff",
+        borderWidth: 2
+    }]
+},
+
+options: {
+    responsive: true
+}}
+);
 
 }
 
 
+function receberData(tipo){
+    let data = new Date()
+    let diaTemp = data.getDate()
+    let mesTemp = data.getMonth()+1
+    let anoTemp = data.getFullYear()
+    if(tipo=="full"){
+    return [diaTemp,mesTemp,anoTemp]
+}
+else if(tipo=="mesFormatado"){
+    return mesTemp+"/"+anoTemp  
+}
+else if(tipo=="dia"){
+  
+    return diaTemp
+}
+else if(tipo=="ano"){
+  
+    return anoTemp
+}
+else if(tipo == "qtdDias"){
+    return new Date(anoTemp,mesTemp,0).getDate()
+}
+}
+
+
+function criarLabel(mest,anot){
+    let mesFormatado = receberData("mesFormatado")
+    let data = receberData("qtdDias")
+    console.log(mest)
+    for(let i=1;i<=data;i++){
+    label.push(i)
+    }
+    return label
+}
+
+
+function atualizarCanvas(valor,dataEscolhida,buscar){
+     let dia
+     let mesFormatado
+    if(!buscar){
+ dia = dataEscolhida[0]
+  mesFormatado = dataEscolhida[1]+"/"+dataEscolhida[2]
+ alert(mesFormatado)
+    }
+    if(buscar){
+      mesFormatado = dataEscolhida[0]+"/"+dataEscolhida[1]
+
+    }
+    let data =  new Date(dataEscolhida[1],dataEscolhida[0],0).getDate()
+   
+     
+    console.log(mesFormatado,"tst")
+    let vendas=JSON.parse(localStorage.getItem("vendas"+mesFormatado))
+    let saidas = JSON.parse(localStorage.getItem("saidas"+mesFormatado))
+    let tipoPagamento = document.getElementById("formaDePagamentoSelect").value
+    if(!buscar){
+if(vendas==null && valor>0){
+let valores = []
+
+    for(let i =0; i<data;i++){
+        if(i+1!=dia)valores.push(0)
+        else if(i+1==dia)valores.push(valor)
+    }
+
+localStorage.setItem("vendas"+mesFormatado,JSON.stringify(valores))
+atualizarCanvas(valor)
+}
+
+if(saidas==null && valor<0){
+let valores = []
+
+    for(let i =0; i<data;i++){
+        if(i+1!=dia)valores.push(0)
+        else if(i+1==dia)valores.push(valor)
+    }
+localStorage.setItem("saidas"+mesFormatado,JSON.stringify(valores))
+
+
+}
+
+if(valor>0){
+    
+  for(i = 0;i<data;i++){
+    if(i+1==dia)vendas[i]=valor+Number(vendas[i])
+  }
+localStorage.setItem("vendas"+mesFormatado,JSON.stringify(vendas))
+meuGraficoFaturamento.data.datasets[2].data = vendas;
+}
+if(valor<0){
+    
+  for(i = 0;i<data;i++){
+    if(i+1==dia)saidas[i]+=valor
+  }
+meuGraficoFaturamento.data.datasets[1].data = saidas
+localStorage.setItem("saidas"+mesFormatado,JSON.stringify(saidas))
+
+
+}
+let valorDoTipo = Number(localStorage.getItem(tipoPagamento+mesFormatado)||0)
+console.log(valorDoTipo+tipoPagamento)
+localStorage.setItem(tipoPagamento+mesFormatado,valorDoTipo+valor)
+
+
+graficoFormaPG.data.datasets[0].data= [localStorage.getItem("pix"+mesFormatado)||0,localStorage.getItem("credito"+mesFormatado)|| 0,localStorage.getItem("dinheiro"+mesFormatado)|| 0,localStorage.getItem("debito"+mesFormatado)||0]
+
+    }
+else if(buscar){
+    console.log(JSON.parse(localStorage.getItem("vendas"+mesFormatado)))
+meuGraficoFaturamento.data.datasets[1].data = JSON.parse(localStorage.getItem("saidas"+mesFormatado))
+meuGraficoFaturamento.data.datasets[2].data = JSON.parse(localStorage.getItem("vendas"+mesFormatado))
+  
+   graficoFormaPG.data.datasets[0].data= [localStorage.getItem("pix"+mesFormatado)||0,localStorage.getItem("credito"+mesFormatado)|| 0,localStorage.getItem("dinheiro"+mesFormatado)|| 0,localStorage.getItem("debito"+mesFormatado)||0] 
+}    
+    meuGraficoFaturamento.update()
+    graficoFormaPG.update()
+return 0
+}
+
+
+function salvar(){
+let inputValor = Number(document.getElementById("iptValor").value)
+if(verificaNumber(inputValor)){
+
+atualizarCanvas(inputValor,receberData("full"),false)
+}
+}
+
+
+function verificaNumber(x){
+if(isNaN(x)){
+    alert("Digite um número válido")
+    return false
+}
+else if(!isNaN(x))return true
+}
+
+
+function buscar(){
+let mesFormatado = document.getElementById("iptValor").value
+let [ano,mes] = mesFormatado.split("-")
+
+mesFormatado = Number(mes)+"/"+ano
+
+console.log(mesFormatado,"15151")
+
+atualizarCanvas(0,mesFormatado.split("/"),true)
+}
